dcc760d921f5217829107eb48bf5cf22bb0a607d
Pawel <pawel.pechta@edu.uekat.pl>
2021-10-24 21:59:15 +0200

    Renaming variables

--- labs3.py
+++ labs3.py
@@ -1,4 +1,4 @@
-'''
+
 #Zad1
 def zwracam_stringa(name: str, surname: str) -> str:
      return (f'Hi {name} {surname}!')
@@ -28,7 +28,6 @@ elif result == False:
     print("Liczba nieparzysta")
 
 #Zad4
-
 def spr_sumy(num_1: int, num_2: int, num_3: int) -> bool:
     if num_1+num_2 >= num_3:
         return True
@@ -40,7 +39,6 @@ print(spr_sumy(1,2,4))
 
 
 #Zad5
-
 def sprawdzam_liste(arg1: list, arg2: int) -> bool:
     if arg2 in arg1:
         return True
@@ -48,17 +46,17 @@ def sprawdzam_liste(arg1: list, arg2: int) -> bool:
         return False
 
 print(sprawdzam_liste([1,2,3],2))
-'''
+
 
 
 #Zad6
-
 def operacje_na_listach(list1: list, list2: list) -> list:
    temporary_list=list1 + list2 
-   temporary_list1=list(set(temporary_list))
-   temporary_list1 = [x**3 for x in temporary_list]
-   #for i in temporary_list1:
-       #print(i**3) lub wersja z for loop
-   return (temporary_list1)
+   temporary_list=list(set(temporary_list))
+   final_list= [i**3 for i in temporary_list]
+   #for i in temporary_list:
+       #print(i**3) lub wersja z for loop zwracają tylko wartości
+   return (final_list)
 
-print(operacje_na_listach([1,2,3],[0]))
+print(operacje_na_listach([1,2,3],[1,2,4]))
+#operacje_na_listach([1,2,3],[1,2,4])
