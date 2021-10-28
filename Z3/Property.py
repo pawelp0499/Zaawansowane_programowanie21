9c5ededdb4b4d6fce544955cc38f2e36fa2d211a
Pawel <pawel.pechta@edu.uekat.pl>
2021-10-28 19:07:50 +0200

    class Property

---
+++ Z3/Property.py
@@ -0,0 +1,6 @@
+class Property: #parent class
+   def __init__(self, area: int, rooms: int, price: int, address: str):
+       self._area=area
+       self._rooms=rooms
+       self._price=price
+       self._address=address
