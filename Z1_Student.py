f2dfca0efb05309d334d20106ff4d4fc1e30ff07
Pawel <pawel.pechta@edu.uekat.pl>
2021-11-06 20:43:10 +0100

    Improved the code using pycodestyle

--- Z1_Student.py
+++ Z1_Student.py
@@ -1,16 +1,14 @@
-#Zad1
-
 class Student:
-   def __init__(self, name: str, marks: int):
-       self.name=name
-       self.marks=marks
-   def is_passed(self) -> bool:
-      if self.marks > 50:
-          return True
-      else:
-          return False
+    def __init__(self, name: str, marks: float):
+        self._name = name
+        self._marks = marks
 
-false_object = Student("Student X",49)
-true_object = Student("Student Y",51)
-print(false_object.is_passed())
-print(true_object.is_passed())
+    def is_passed(self) -> bool:
+        return True if self._marks > 50 else False
+
+
+student1 = Student("Adam", 45.5)
+student2 = Student("Pawel", 51)
+
+print(student1.is_passed())
+print(student2.is_passed())
