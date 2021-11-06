f71aee85014c0b025689d27d51a86e1497206e0c
Pawel <pawel.pechta@edu.uekat.pl>
2021-11-07 00:22:21 +0100

    deleted

--- zad_2/Student.py
+++
@@ -1,7 +0,0 @@
-class Student:
-    def __init__(self, name: str, marks: float):
-        self.name = name
-        self._marks = marks
-
-    def __str__(self):
-        return f"Student's name is {self.name}, score: {self._marks}."
