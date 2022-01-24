f8e9e017a9a1cc344c6ca11488df2c155d911e80
Pawel <pawel.pechta@edu.uekat.pl>
2022-01-24 08:01:51 +0100

    live detection with camera

---
+++ detectors/video_capture.py
@@ -0,0 +1,48 @@
+import cv2
+import sys
+
+
+cascPath = sys.argv[0]
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                    'haarcascade_frontalface_default.xml')
+
+video_capture = cv2.VideoCapture(0)
+
+
+def camera_detector():
+    while True:
+        ret, vision = video_capture.read()
+
+        gray = cv2.cvtColor(vision, cv2.COLOR_BGR2GRAY)
+
+        human = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        person = 1
+        cv2.putText(vision, f'Number of person: ', (40, 70),
+                    cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 0), 1)
+
+        for (x, y, w, h) in human:
+
+            cv2.rectangle(vision, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(vision, f'person {person}', (x, y),
+                        cv2.FONT_HERSHEY_DUPLEX,
+                        0.63, (0, 0, 255), 1)
+            person += 1
+
+            cv2.putText(vision, f'{person - 1}', (295, 70),
+                        cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 1)
+        cv2.imshow('result', vision)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+camera_detector()
+video_capture.release()
+cv2.destroyAllWindows()
